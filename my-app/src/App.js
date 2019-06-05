92c6244d9107bc7c95db894cfe0cd96066fc86f8
chenrugang <641838487@qq.com>
2019-06-05 16:49:17 +0800

    tj

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,4 +18,23 @@ class App extends Component {
   }
 }
 
+// class Clock extends Component {
+// 	render() {
+// 	  return (
+// 		<div>
+// 		  <h1>Hello, world!</h1>
+// 		  <h2>现在是 {this.props.date.toLocaleTimeString()}.</h2>
+// 		</div>
+// 	  );
+// 	}
+//   }
+   
+// function tick() {
+// 	ReactDOM.render(
+// 	  <Clock date={new Date()} />,
+// 	  document.getElementById('example')
+// 	);
+// }
+   
+// setInterval(tick, 1000);
 export default App;
