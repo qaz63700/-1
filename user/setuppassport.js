c324143684f0a698260c589da749b6ff0cd86f66
chenrugang <641838487@qq.com>
2019-06-10 19:44:26 +0800

    tj

--- user/setuppassport.js
+++ user/setuppassport.js
@@ -7,7 +7,6 @@ passport.use("login", new LocalStrategy(function(username, password, done) {
         if (!user) {
             return done(null, false, { message: "No user has that username!" });
         }
-        
         user.checkPassword(password, function(err, isMatch) {
             if (err) { return done(err); }
             if (isMatch) {
