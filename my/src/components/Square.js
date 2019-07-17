ec73b6a043c158d99d72a0d2b72e7c8c1f6346f9
chenrugang <641838487@qq.com>
2019-07-17 18:17:18 +0800

    tj

---
+++ my/src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Square extends React.Component{
+    render(){
+        return(
+            <button className='square' onClick={function() { alert('click'); }}>
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+export default Square;
