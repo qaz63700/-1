5e93f623c722d3838b62753b5621b9a57b7796a2
chenrugang <641838487@qq.com>
2019-07-19 19:06:51 +0800

    tj

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Button from 'antd/es/button';
+import Games from './Games'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+		<div>
+			<Games/>
+			<br />
+			<Button type="primary" shape="circle" icon="search" />
+			<Button type="primary" icon="search">
+			Search
+			</Button>
+			<Button shape="circle" icon="search" />
+			<Button icon="search">Search</Button>
+			<br />
+			<Button shape="circle" icon="search" />
+			<Button icon="search">Search</Button>
+			<Button type="dashed" shape="circle" icon="search" />
+			<Button type="dashed" icon="search">
+			Search
+			</Button>
+	  </div>
+    );
+  }
 }
 
 export default App;
