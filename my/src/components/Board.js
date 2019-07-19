4af44ddbdec78d77a6c315037bdbbf8671f7c147
chenrugang <641838487@qq.com>
2019-07-19 09:22:03 +0800

    tj

--- my/src/components/Board.js
+++ my/src/components/Board.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import Square from './Square';
 
+
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i}/>;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
     }
+  
     render() {
-        const status = 'Next player: X';
-
-        return (
+      return (
         <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
+          <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
+          </div>
+          <div className="board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
+          </div>
+          <div className="board-row">
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
-            </div>
+          </div>
         </div>
-        );
+      );
     }
-}
-
+  }
 export default Board;
